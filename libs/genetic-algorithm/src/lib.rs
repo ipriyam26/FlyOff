724bc32e7f0d8a93250abe3bf4e7e225087eb075
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-01-03 22:36:20 +0530

    SOme work

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,17 +1,92 @@
-pub struct  GeneticAlgorithm;
+use rand::{seq::SliceRandom, RngCore};
 
-impl GenetricAlgorithm {
-    pub fn new() -> Self{
+pub struct GeneticAlgorithm;
+
+impl GeneticAlgorithm {
+    pub fn new() -> Self {
         Self
     }
-    pub fn evolve<I>(&self,population:&[I])->Vec<I>{
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
         // we cannot evolve an empty population
         assert!(!population.is_empty());
 
-        (0..population.len())
-        .map(|_|{
-            todo!()
-        }).collect()
+        (0..population.len()).map(|_| todo!()).collect()
+    }
+}
+
+pub trait Individual {
+    fn fitness(&self) -> f32;
+}
+
+pub struct RouletteWheelSelection;
+
+impl RouletteWheelSelection {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+
+pub trait SelectionMethod {
+    fn select<'a, I>(
+       &self,
+       rng: &mut dyn RngCore,
+       population: &'a [I],
+    ) -> &'a I
+    where
+        I: Individual;
+}
+
+impl  SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("Got an empty population")
+    }
+}
+
+
+#[cfg(test)]
+#[derive(Clone, Debug)]
+pub struct TestIndividual {
+    fitness: f32,
+}
+
+#[cfg(test)]
+impl TestIndividual {
+    pub fn new(fitness: f32) -> Self {
+        Self { fitness }
+    }
+}
+
+#[cfg(test)]
+impl Individual for TestIndividual {
+    fn fitness(&self) -> f32 {
+        self.fitness
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    use super::*;
+
+    #[test]
+    fn test(){
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+        
 
     }
+
 }
