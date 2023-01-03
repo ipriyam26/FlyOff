eda4b49487135ddf6dbad930238a460533381e09
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-01-03 13:58:42 +0530

    Neural Network

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,72 @@
+pub struct  Network{
+layers:Vec<Layer>
+}
+
+struct  Neuron{
+    bias:f32,
+    weights:Vec<f32>
+}
+struct  Layer{
+    neurons:Vec<Neuron>,
+}
+
+impl  Network  {
+    pub fn propogate(&self, inputs:Vec<f32>)->Vec<f32>{
+        // we pass the input through each layer and return the output from the last layer back to the caller
+        self.layers
+        .iter()
+        .fold(inputs,
+             |inputs,layer|
+             layer.propogate(inputs)
+            )
+    }
+    pub fn random(layers: Vec<LayerTopology>) -> Self {
+        let layers = layers
+        .windows(2)
+        .map(|layers|{
+Layer::random(layers[0].neurons, layers[1].neurons)
+        }).collect();
+        Self { layers: layers }
+    }
+    
+}
+
+
+impl  Neuron {
+    fn propogate(&self,inputs:&Vec<f32>) -> f32{
+let output = inputs
+.iter()
+.zip(&self.weights)
+.map(|(input,weight)| input*weight)
+.sum::<f32>();
+(self.bias+output).max(0.0)
+    }
+}
+
+pub struct  LayerTopology{
+    neurons:usize
+}
+
+impl  Layer {
+    
+    fn propogate(&self,inputs:Vec<f32>) -> Vec<f32>{
+       self.neurons
+       .iter()
+       .map(|neuron| neuron.propogate(&inputs))
+       .collect()
+    }
+    pub fn random(
+        input_neurons: usize,
+        output_neurons: usize,
+    ) -> Self {
+        let mut neurons = Vec::new();
+
+        for _ in 0..output_neurons {
+            neurons.push(Neuron::random(input_neurons));
+        }
+
+        Self { neurons }
+    }
+
+}
+
---
+++ target/debug/deps/neural_network-cd699f6aa2d62db0.d
@@ -0,0 +1,5 @@
+/Users/ipriyam26/Programing/Genetic/food_search/target/debug/deps/neural_network-cd699f6aa2d62db0.rmeta: libs/neural-network/src/lib.rs
+
+/Users/ipriyam26/Programing/Genetic/food_search/target/debug/deps/neural_network-cd699f6aa2d62db0.d: libs/neural-network/src/lib.rs
+
+libs/neural-network/src/lib.rs:
